777d237167ecf1868ccfbee350863f94232e5bbc
Joyson3008 <rjoyson30@gmail.com>
2026-04-08 18:08:03 +0530

    health

---
+++ src/test/java/com/joyson/ai_life_tracker/HealthController.java
@@ -0,0 +1,13 @@
+package com.joyson.ai_life_tracker;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() { 
+        return "OK";
+    }
+}
